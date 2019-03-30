a15ccb485b50a5592f4c7903cdebd52abacda2a1
michael.cottow <michael.cottow@thomsonreuters.com>
2019-03-30 19:47:30 -0400

    test.c

--- test.c
+++ test.c
@@ -3,37 +3,49 @@
 #include <string.h>
 
 #define INPUTFILE "accountHistory.txt" 
-#define OUTPUTFILE "accountHistory.txt" 
-
+#define OUTPUTFILE "accountHistory1.txt" 
 
 struct node {
-char name[50];
-char amount[12];
-struct node *next;
+		char name[50];
+		char amount[12];
+		struct node *next;
 }*head;
 
-int main(void) {
 
-	struct node *temp, *last;
-	temp = malloc(sizeof(struct node));
-	last = malloc(sizeof(struct node));
-	head = temp;
+void addEntry(struct node * head, char* name, char* amount){
+	struct node * current = head; 
 
+	while (current->next != NULL){
+		current = current->next;
+	}
+
+	struct node * newNode = malloc(sizeof(struct node));
+
+	current->next = newNode;
+	newNode->next = NULL;
+	strcpy(newNode->name, name);
+	strcpy(newNode->amount, amount);
+
+
+}
+
+int loadList(struct node *temp)
+{
 	FILE *ifp;
 	char buffer[1024];
 	memset(buffer, 0, 1024);
 
-	if ((ifp = fopen(INPUTFILE, "r")) == NULL){
+	if ((ifp = fopen(INPUTFILE, "r")) == NULL) {
 		printf("\nError with the input file: %s", INPUTFILE);
 		puts("\nPress enter to continue...");
 		getchar();
 		return 1;
 	}
 
-	if (fgets(buffer, 1024, ifp)==NULL) {
+	if (fgets(buffer, 1024, ifp) == NULL) {
 		fclose(ifp);
 		printf("\nThe input file is empty: %s", INPUTFILE);
-		puts("\nPress enter to continue...");
+		puts(".\nPress enter to continue...");
 		getchar();
 		return 1;
 	}
@@ -43,7 +55,6 @@ int main(void) {
 	strcpy(temp->amount, strtok(NULL, ";"));
 	temp->next = NULL;
 
-
 	while (fgets(buffer, 1024, ifp)) {
 		temp->next = malloc(sizeof(struct node));
 		temp = temp->next;
@@ -51,29 +62,39 @@ int main(void) {
 		strcpy(temp->name, strtok(buffer, ";"));
 		strcpy(temp->amount, strtok(NULL, ";"));
 		temp->next = NULL;
-		last = temp;
 	}
 
-	struct node * newNode = malloc(sizeof(struct node));
-	newNode->next = NULL;
-	strcpy(newNode->name, "Alex Cottow");
-	strcpy(newNode->amount, "555");
-	last -> next = newNode;
+fclose(ifp);
 
-	fclose(ifp);
+return 0;
+}
 
 
 
+int main(void) {
+
+	struct node *temp;
+	temp = malloc(sizeof(struct node));
+	head = temp;
+
+	if (loadList(head) != 0) {
+		printf("\nThe list was not loaded from the input file: %s", INPUTFILE);
+		puts(".\nPress enter to continue...");
+		getchar();
+		return 1;
+	}
+	addEntry(head, "Alex", "55555");
+	
 	printf("\nSCANNING THE LIST\n");
 	temp = head;
 	int i = 0;
+
 	while (temp != NULL) {
 		i ++ ;
 		printf("#%d, Name:%s, Amount:%s", i, temp->name, temp->amount);
 		//	total += n->balance;
 		temp = temp->next;
 	}
-
 
 	printf("\n");
 	getchar();
